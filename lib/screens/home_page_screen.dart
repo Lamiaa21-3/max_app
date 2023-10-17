960ad49821031285ab47b478e20cd4752cdb86c5
Lamiaa21-3 <lamiaagamal213@gmail.com>
2023-10-17 13:44:28 +0200

    cart screen

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_app/cubit/homePage_cubit/homePage_cubit.dart';
 import 'package:new_app/cubit/login_cubit/login_cubit.dart';
 import 'package:new_app/cubit/login_cubit/login_state.dart';
+import 'package:new_app/screens/cart_screen.dart';
 
 import '../components/constants.dart';
 
@@ -12,12 +13,12 @@ class HomePageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        HomePageCubit.get(context).getHomePage();
-        print('object');
+    HomePageCubit.get(context).getHomePage();
+    print('object');
     print(LogInCubit.get(context).autherModel?.userName);
-    return BlocConsumer<LogInCubit,LogInStates>(
-      listener: (context,state) {},
-      builder: (context,state) {
+    return BlocConsumer<LogInCubit, LogInStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.white,
@@ -30,51 +31,54 @@ class HomePageScreen extends StatelessWidget {
                 color: Colors.black,
               ),
             ),
+            actions: [
+              IconButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> CartScreen()));
+              }, icon: Icon(Icons.shopping_cart),color: Colors.black,),
+            ],
             elevation: 0,
-            title: Text('HomePage', style: TextStyle(color: Colors.black),),
-          ),
-          body:GridView.builder(
-            gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
+            title: Text(
+              'HomePage',
+              style: TextStyle(color: Colors.black),
             ),
-            itemCount: HomePageCubit.get(context).homePageList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.teal,
-
-                        ),
-                        child: Center(child: Text('${HomePageCubit.get(context).homePageList[index].product?.nameInEnglish}')),
-                      ),
-                      Image.network(
-                          'https://ecommerce.6sigmacode.com${HomePageCubit.get(context).homePageList[index].product?.mainImage}',height: 120,),
-                      Center(child: Text('${HomePageCubit.get(context).homePageList[index].date}')),
-                    ],
-                  ),
-                ),
-              );
-            }
-
-
-
           ),
-
+          body: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: HomePageCubit.get(context).homePageList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.teal,
+                          ),
+                          child: Center(
+                              child: Text(
+                                  '${HomePageCubit.get(context).homePageList[index].product?.nameInEnglish}')),
+                        ),
+                        Image.network(
+                          'https://ecommerce.6sigmacode.com${HomePageCubit.get(context).homePageList[index].product?.mainImage}',
+                          height: 120,
+                        ),
+                        Center(
+                            child: Text(
+                                '${HomePageCubit.get(context).homePageList[index].date}')),
+                      ],
+                    ),
+                  ),
+                );
+              }),
         );
       },
     );
   }
-      }
-
-
-
-
-
+}
